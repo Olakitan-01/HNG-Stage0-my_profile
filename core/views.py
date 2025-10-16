a75e85d0da3c03aac5805674943294ccaa070ff2
Olanrewaju <olanrewajuemma2023@gmail.com>
2025-10-16 16:56:41 +0100

    stage0-task

---
+++ core/views.py
@@ -0,0 +1,30 @@
+import requests
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import JsonResponse
+from datetime import datetime, timezone
+
+
+def my_profile(request):
+    user = {"email": "olanrewajuemma2023@gmail.com", 
+            "name": "OLANREWAJU Emmanuel",
+            "stack":"Python(django)"}
+    timestamp = datetime.now(timezone.utc).isoformat()
+
+    try:
+        response = requests.get("https://catfact.ninja/fact", timeout=5)
+        response.raise_for_status()
+        data = response.json()
+        fact = data.get("fact", "Cats are...")
+    except requests.exceptions.RequestException:
+        fact = "could not fetch cat fact at the moment"
+
+    result = {
+        "status": "success",
+        "user": user,
+        "timestamp": timestamp,
+        "fact": fact
+    }
+    return JsonResponse(result, content_type="application/json")
